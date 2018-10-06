1a9c23364dc44f7eb31c9e83878353a7fda524ec
fercho <ferchofpz@hotmail.com>
2018-10-06 16:13:47 -0500

    frontend

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { LoginPage } from '../pages/login/login';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+
+import { HttpClientModule  } from "@angular/common/http";
+import { RegistroPage } from '../pages/registro/registro';
+
+import { GoogleMaps } from '@ionic-native/google-maps';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage,
+    LoginPage,
+    RegistroPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage,
+    LoginPage,
+    RegistroPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    AuthServiceProvider,
+    GoogleMaps,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
